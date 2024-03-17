1e275160e828636afdf516b7ec8c1d7ac781acce
matthewfernst <matthew.f.ernst@gmail.com>
2024-03-17 12:40:43 -0700

    first

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,30 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"mfussenegger/nvim-dap-python",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+    require("dapui").setup()
+    require("dap-python").setup("/Users/matthewernst/miniconda3/envs/mlx-env/bin/python")
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+	end,
+}
