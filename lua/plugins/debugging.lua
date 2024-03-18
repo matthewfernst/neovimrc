b61ac9b57dfc90cbda956d4268a2711fcd940c5e
matthewfernst <matthew.f.ernst@gmail.com>
2024-03-17 22:09:15 -0700

    debugging configs
    Signed-off-by: matthewfernst <matthew.f.ernst@gmail.com>

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -5,9 +5,54 @@ return {
 		"rcarriga/nvim-dap-ui",
 	},
 	config = function()
-    require("dapui").setup()
-    require("dap-python").setup("/Users/matthewernst/miniconda3/envs/mlx-env/bin/python")
+		require("dapui").setup()
+		require("dap-python").setup("/Users/matthewernst/miniconda3/envs/mlx-env/bin/python")
 		local dap, dapui = require("dap"), require("dapui")
+
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/usr/bin/lldb",
+			name = "lldb",
+		}
+
+		dap.configurations.cpp = {
+			-- Example
+			{
+				name = "graph_test",
+				type = "lldb",
+				request = "launch",
+				program = "graph_test",
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
+
+		dap.configurations.python = {
+			{
+				-- The first three options are required by nvim-dap
+				type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+				request = "launch",
+				name = "Launch file",
+
+				-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+				program = "${file}", -- This configuration will launch the current file if used.
+				pythonPath = function()
+					-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+					-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+					-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+					local cwd = vim.fn.getcwd()
+					if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+						return cwd .. "/venv/bin/python"
+					elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+						return cwd .. "/.venv/bin/python"
+					else
+						return "/usr/bin/python"
+					end
+				end,
+			},
+		}
 
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
