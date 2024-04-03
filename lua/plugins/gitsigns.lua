fabe19be41bb3d3d8c3b7727ef97f4ebb53bdc6e
matthewfernst <matthew.f.ernst@gmail.com>
2024-04-02 17:29:58 -0700

    fix: keymaps for Gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -44,5 +44,7 @@ return {
         enable = false,
       },
     })
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
   end,
 }
